6a4d8654b463f13e13f80bdebeebe7265b0dc79c
nonadev <wesley.vieira.lima.1999@gmail.com>
2023-05-08 14:34:17 -0300

    Adicionado grid para atualizar posicao e salvar apenas 1 por mark2d

--- src/main/Board/fix_point.gd
+++ src/main/Board/fix_point.gd
@@ -1,14 +1,29 @@
 extends Marker2D
 class_name FixPoint
 
+#@export var occupied: bool = false
+
+var currentPawn = null
+
+
 func _on_draw():
 	draw_circle(Vector2.ZERO, 5, Color.BLANCHED_ALMOND)
 
 
-func select():
+func select(pawn):
 	for child in get_tree().get_nodes_in_group("zone"):
-		child.deselect()
+		if(pawn == child.currentPawn):
+			child.deselect()
+			child.selectNextInventoryEmpty()
+
+	currentPawn = pawn
 	modulate = Color.RED
+
 
 func deselect():
 	modulate = Color.WHITE
+	currentPawn = null
+
+
+func selectNextInventoryEmpty():
+	print("Ainda precisa ser implementado")
--- src/main/BuyablePieces/pawn_buy.gd
+++ src/main/BuyablePieces/pawn_buy.gd
@@ -5,10 +5,18 @@ var rest_point
 var rest_nodes = []
 
 
+func _ready():
+	rest_nodes = get_tree().get_nodes_in_group("zone")
+	rest_point = rest_nodes[0].global_position
+	rest_nodes[0].select(self)
+
+
 func _physics_process(delta):
 	if dragging:
 		var mousepos = get_global_mouse_position()
-		position = lerp(global_position, mousepos, 25 * delta)
+		global_position = lerp(global_position, mousepos, 20 * delta)
+	else:
+		global_position = lerp(global_position, rest_point, 5 * delta)
 
 
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int):
@@ -17,3 +25,9 @@ func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int):
 			dragging = true
 		if (event.button_index == MOUSE_BUTTON_LEFT and !event.pressed):
 			dragging = false
+			var shortest_dist = 6
+			for child in rest_nodes:
+				var distance = global_position.distance_to(child.global_position)
+				if(distance < shortest_dist):
+					child.select(self)
+					rest_point = child.global_position
