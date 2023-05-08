407412259fd6a544a1633c2185a3283b76fa2e44
nonadev <wesley.vieira.lima.1999@gmail.com>
2023-05-08 19:53:16 -0300

    Removido traços de debug e marcando quando pode ou não ser usado

--- src/main/Board/fix_point.gd
+++ src/main/Board/fix_point.gd
@@ -1,29 +1,28 @@
 extends Marker2D
 class_name FixPoint
 
-#@export var occupied: bool = false
+@export var canBeUsed: bool = true
 
 var currentPawn = null
 
 
 func _on_draw():
-	draw_circle(Vector2.ZERO, 5, Color.BLANCHED_ALMOND)
+	# draw_circle(Vector2.ZERO, 5, Color.BLANCHED_ALMOND)
+	pass
 
 
 func select(pawn):
-	for child in get_tree().get_nodes_in_group("zone"):
-		if(pawn == child.currentPawn):
-			child.deselect()
-			child.selectNextInventoryEmpty()
+	if(canBeUsed):
+		canBeUsed = false
+		for child in get_tree().get_nodes_in_group("zone"):
+			if(pawn == child.currentPawn):
+				child.deselect()
 
-	currentPawn = pawn
-	modulate = Color.RED
+		currentPawn = pawn
+		# modulate = Color.RED
 
 
 func deselect():
-	modulate = Color.WHITE
+	# modulate = Color.WHITE
+	canBeUsed = true
 	currentPawn = null
-
-
-func selectNextInventoryEmpty():
-	print("Ainda precisa ser implementado")
