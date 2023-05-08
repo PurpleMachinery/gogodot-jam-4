035d1bc7890299f433c5e83cef82a736068623c7
nonadev <wesley.vieira.lima.1999@gmail.com>
2023-05-07 21:43:50 -0300

    Pawn tem animação de ataque e troca de cor e vai voltando com o tempo

--- src/main/ChessPieces/Pawn.gd
+++ src/main/ChessPieces/Pawn.gd
@@ -1,10 +1,14 @@
 extends Node2D
 class_name Pawn
 
+@onready var animationPlayer: AnimationPlayer = $AnimationPlayer
+@onready var sprite: Sprite2D = $Sprite2D
 @onready var rayCastLeft: RayCast2D = $RayCast2DLeft
 @onready var rayCastRight: RayCast2D = $RayCast2DRight
 
 @export var damage: int = 1
+
+var tempCanPrint = false
 
 var attackTimer: Timer = Timer.new()
 
@@ -14,11 +18,23 @@ func _ready():
 
 
 func _process(_delta):
+	var correctColor = lerp(sprite.modulate.g, 1.0, 0.0065)
+	sprite.modulate = Color(1, correctColor, correctColor, 1)
+
+	var enemyLeft: bool = true
 	var firstEnemy = rayCastLeft.get_collider()
 
 	if(firstEnemy == null):
+		enemyLeft = false
 		firstEnemy = rayCastRight.get_collider()
 	
 	if(attackTimer.is_stopped() && firstEnemy != null):
+		if(enemyLeft):
+			animationPlayer.play("attack_left")
+		else:
+			animationPlayer.play("attack_right")
+
 		firstEnemy.get_owner().dealDamage(damage)
+		sprite.modulate = Color(1, 0, 0, 1)
 		attackTimer.start(4)
+		tempCanPrint =true
