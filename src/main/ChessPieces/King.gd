0afce41cf3c162cd46741d2392c5fe7290c2ef1c
nonadev <wesley.vieira.lima.1999@gmail.com>
2023-05-15 20:58:54 -0300

    Adicionado sons no rei

--- src/main/ChessPieces/King.gd
+++ src/main/ChessPieces/King.gd
@@ -1,9 +1,23 @@
 extends Node2D
 
-@export var health: int = 20;
+
+@onready var audioPlayerBefore: AudioStreamPlayer = $AudioStreamPlayer_before_die
+@onready var audioPlayerHurt: AudioStreamPlayer = $AudioStreamPlayer_hurt
+@onready var audioPlayerAfter: AudioStreamPlayer = $AudioStreamPlayer_after_die
+
+@export var health: int = 5;
+
 
 func dealDamage(damage: int):
-    health -= damage
+	health -= damage
+	if(health <= 0):
+		audioPlayerBefore.play()
+		await audioPlayerBefore.finished
+		audioPlayerHurt.play()
+		await audioPlayerHurt.finished
+		audioPlayerAfter.play()
+		await audioPlayerAfter.finished
 
-    if(health <= 0):
-        get_tree().change_scene_to_file("res://src/main/game_over.tscn")
+		get_tree().change_scene_to_file("res://src/main/game_over.tscn")
+	else:
+		audioPlayerHurt.play()
